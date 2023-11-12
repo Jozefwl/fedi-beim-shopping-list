3448c036fb693ac18ab2816acd96800c882fccb1
Jozef W <62598812+Jozefwl@users.noreply.github.com>
2023-11-12 14:03:26 +0100

    Revert "Comments"
    This reverts commit f87efbaebd8ec1795872a798b5d3ad7c51a6aac7.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,76 +1,31 @@
-// Importing necessary dependencies
-import React, { useState } from "react";
-import Modal from "react-modal";
-import "../styles/LoginModal.css";
+import React from "react";
+import "../styles/Navbar.css";
 
-// Setting the root element for the modal in the application
-Modal.setAppElement("#root");
-
-// Functional component for the Login Modal
-const LoginModal = ({ isOpen, onRequestClose, onLogin }) => {
-  // State to manage the username input
-  const [username, setUsername] = useState("");
-  // State to manage the password input
-  const [password, setPassword] = useState("");
-
-  // Function to handle the login button click
-  const handleLogin = () => {
-    // Callback function to pass the username and password to the parent component
-    onLogin(username, password);
-  };
-
-  // Function to handle changes in the username input
-  const handleUsernameChange = (e) => {
-    // Updating the state with the new username value
-    setUsername(e.target.value);
-  };
-
-  // Function to handle changes in the password input
-  const handlePasswordChange = (e) => {
-    // Updating the state with the new password value
-    setPassword(e.target.value);
-  };
-
-  // JSX structure for the Login Modal
+const Navbar = ({ username, onLoginClick, onLogoutClick }) => {
   return (
-    <Modal
-      isOpen={isOpen}  // Prop to determine whether the modal is open or closed
-      onRequestClose={onRequestClose}  // Callback function to close the modal
-      className="modal"  // Class name for styling the modal content
-      overlayClassName="overlay"  // Class name for styling the modal overlay
-    >
-      {/* Close button for the modal */}
-      <button className="close-btn" onClick={onRequestClose}>
-        X
-      </button>
-      {/* Content of the modal */}
-      <div className="modal-body">
-        {/* Input for the username */}
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={handleUsernameChange}  // Input change handler
-          className="inputBox"  // Class name for styling the input
-        />
-        {/* Input for the password */}
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={handlePasswordChange}  // Input change handler
-        />
+    <nav className="navbar">
+    
+      <div className="navbar-left">
+        {username && <h1 className="navbar-text">Welcome {username}</h1>}
+        {!username && <h1 className="navbar-text">Please log in</h1>}
       </div>
-      {/* Footer section of the modal */}
-      <div className="modal-footer">
-        {/* Button to trigger the login functionality */}
-        <button className="login-btn" onClick={handleLogin}>
-          Login
-        </button>
+      <div className="navbar-center">
+        <a href="/"><button className="navbar-home">Home</button></a>
+        </div>
+      <div className="navbar-right">
+        {username && (
+          <button className="navbar-btn" onClick={onLogoutClick}>
+            Logout
+          </button>
+        )}
+        {!username && (
+          <button className="navbar-btn" onClick={onLoginClick}>
+            Login
+          </button>
+        )}
       </div>
-    </Modal>
+    </nav>
   );
 };
 
-// Exporting the LoginModal component as the default export
-export default LoginModal;
+export default Navbar;
--- src/components/UserContext.js
+++ src/components/UserContext.js
@@ -1,11 +1,5 @@
-// Importing the React library
 import React from "react";
 
-// Creating a React context for managing user-related data
 const UserContext = React.createContext("");
 
-// Exporting the UserContext to make it available for other components
 export default UserContext;
-
-
-//TODO: Make it so that the username is a global variable that can be accessed by all components
